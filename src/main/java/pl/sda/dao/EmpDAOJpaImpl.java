19a242cce7f02eebeb4c153b0b109f47e5fde03f
Przydan <patryk@przydanek.com>
2019-12-01 14:37:40 +0100

    # added some jpa implementation

--- src/main/java/pl/sda/dao/EmpDAOJpaImpl.java
+++ src/main/java/pl/sda/dao/EmpDAOJpaImpl.java
@@ -1,8 +1,11 @@
 package pl.sda.dao;
 
+import pl.sda.domain.Department;
 import pl.sda.domain.Employee;
 
+import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
 import java.math.BigDecimal;
 import java.util.List;
 
@@ -18,32 +21,91 @@ public class EmpDAOJpaImpl implements EmpDAO {
 
     @Override
     public Employee findById(int id) {
-        // TODO: implement method
-        return null;
+        EntityManager em = emf.createEntityManager();
+        try {
+            return em.find(Employee.class, id);
+        } finally {
+            em.close();
+        }
     }
 
     @Override
     public void create(Employee employee) {
-        // TODO: implement method
+        EntityManager em = emf.createEntityManager();
+        EntityTransaction tx = null;
 
+        try {
+            tx = em.getTransaction();
+            tx.begin();
+            em.persist(employee);
+            tx.commit();
+        } catch (Exception ex) {
+            if (tx != null && tx.isActive()) {
+                tx.rollback();
+            }
+            throw ex;
+        } finally {
+            em.close();
+        }
     }
 
     @Override
     public void update(Employee employee) {
-        // TODO: implement method
+        EntityManager em = emf.createEntityManager();
+        EntityTransaction tx = null;
+        try {
+            tx = em.getTransaction();
+            tx.begin();
+            em.merge(employee);
+            tx.commit();
+        } catch (Exception ex) {
+            if (tx != null && tx.isActive()) {
+                tx.rollback();
+            }
+            throw ex;
+        } finally {
+            em.close();
+        }
 
     }
 
     @Override
     public void delete(int id) {
-        // TODO: implement method
-
+        EntityManager em = emf.createEntityManager();
+        EntityTransaction tx = null;
+        try {
+            tx = em.getTransaction();
+            tx.begin();
+            Employee emp = em.find(Employee.class, id);
+            em.remove(emp);
+            tx.commit();
+        } catch (Exception ex) {
+            if (tx != null && tx.isActive()) {
+                tx.rollback();
+            }
+            throw ex;
+        } finally {
+            em.close();
+        }
     }
 
     @Override
     public void create(List<Employee> employees) {
-        // TODO: implement method - create all entities in one transaction (all on nothing)
-
+        EntityManager em = emf.createEntityManager();
+        EntityTransaction tx = null;
+        try {
+            tx = em.getTransaction();
+            tx.begin();
+            employees.forEach(em::persist);
+            tx.commit();
+        } catch (Exception ex) {
+            if (tx != null && tx.isActive()) {
+                tx.rollback();
+            }
+            throw ex;
+        } finally {
+            em.close();
+        }
     }
 
     @Override
